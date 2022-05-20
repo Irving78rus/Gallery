c65caa2be1fbecd6237034532b9960d5db2bc505
Irving78rus <isakov911@mail.ru>
2022-05-20 22:56:49 +0300

    css add

--- src/Button.js
+++ src/Button.js
@@ -4,7 +4,8 @@ const Button = ( {type,disabled,onClick,children,isDisable,className}) => {
  
  
   return (
-    <button type={type} disabled={isDisable?disabled:null} onClick={onClick} className={className} style={isDisable?null:{backgroundColor: "rgba(45, 45, 164, 0.651)"}}> 
+    <button type={type} disabled={isDisable?disabled:null} 
+    onClick={onClick} className={className}  > 
   {children}
 </button>
  
